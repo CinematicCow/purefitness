915f1f44230d444797839ccc2561efc85984db5b
CinematicCow <prajwalkakkar@gmail.com>
2021-02-25 13:53:09 +0530

    added navbar and upper background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,43 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Please Wait...</h1>
+    <div id="fh5co-wrapper">
+      <div id="fh5co-page">
+        <Navbar />
+        {/* background image black and upper portion */}
+
+        <div className="fh5co-hero">
+          <div className="fh5co-overlay" />
+          <div
+            className="fh5co-cover"
+            // data-stellar-background-ratio="0.5"
+            style={{
+              backgroundImage: "url(images/home-image.jpg)",
+              backgroundPosition: "center",
+            }}
+          >
+            <div className="desc animate-box">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-7">
+                    <h2>
+                      Pure Fitness &amp; Health <br />
+                      is a <b>Mentality.</b>
+                    </h2>
+                    <br />
+                    <span>
+                      <a className="btn btn-primary" href="#">
+                        Start Your Journey
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end background image black and upper portion */}
+      </div>
     </div>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,40 @@
 function Navbar() {
   return (
-    <div>
-      <nav>brr?</nav>
+    <div id="fh5co-header">
+      <header id="fh5co-header-section">
+        <div className="container">
+          <div className="nav-header">
+            <a href="#" className="js-fh5co-nav-toggle fh5co-nav-toggle">
+              <i />
+            </a>
+            <h1 id="fh5co-logo">
+              <a href="index.html">
+                Pure<span>Fitness</span>
+              </a>
+            </h1>
+            {/* START #fh5co-menu-wrap */}
+            <nav id="fh5co-menu-wrap" role="navigation">
+              <ul className="sf-menu" id="fh5co-primary-menu">
+                <li className="active">
+                  <a href="index.html">Home</a>
+                </li>
+                <li>
+                  <a href="schedule.html">Schedule</a>
+                </li>
+                <li>
+                  <a href="trainer.html">Trainers</a>
+                </li>
+                <li>
+                  <a href="about.html">About</a>
+                </li>
+                <li>
+                  <a href="contact.html">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
