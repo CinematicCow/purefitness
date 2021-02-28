9674f09adf74df79d8a0a55bce0e988f895f672b
CinematicCow <prajwalkakkar@gmail.com>
2021-02-28 21:44:27 +0530

    add trainer route

---
+++ backend/api/database/models/userModel.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose")
+
+const programSchema = new mongoose.Schema({})
+
+module.exports = mongoose.model("Program", programSchema)
--- backend/api/routes/Trainers.js
+++ backend/api/routes/Trainers.js
@@ -1,6 +1,34 @@
 const express = require("express")
-const router = express.Router()
+const trainerRouter = express.Router()
+const Trainer = require("../database/models/trainerModel")
 
-router.get("/", (req, res, next) => {})
+// @desc Get all Trainers
+// @route GET /trainers/
+trainerRouter.get("/", (req, res, next) => {
+  Trainer.find()
+    .then((docs) => {
+      console.log(docs)
+      res.status(200).json(docs)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({
+        error: err,
+      })
+    })
+})
 
-module.exports = router
+// @desc Add new Trainer
+// @route POST /trainers/add
+trainerRouter.post("/add", (req, res, next) => {
+  const params = req.body
+  const trainer = new Trainer({
+    name: params.name,
+    type: params.type,
+    about: params.about,
+    image: params.image,
+    social: [{}],
+  })
+})
+
+module.exports = trainerRouter
