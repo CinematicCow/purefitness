9fd5c324a4352bb420aeb7ddd622421a87d905da
CinematicCow <CinematicCow@users.no-reply.github.com>
2021-04-07 22:46:23 +0530

    add: backend as strapi app

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dfd8c255e92e0589599c4374359067ac'),
+    },
+  },
+});
