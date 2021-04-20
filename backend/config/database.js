128570995d97b2824144bb078cde0acc34c73236
CinematicCow <CinematicCow@users.no-reply.github.com>
2021-04-20 23:22:03 +0530

    added various endpoints to backend

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'pf-dev-backend'),
-        username: env('DATABASE_USERNAME', 'pk'),
-        password: env('DATABASE_PASSWORD', 'evofox'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: env.bool("DATABASE_SSL"),
       },
-      options: {}
+      options: {},
     },
   },
 });
