eca6099d1642e067b215108be6ce7b8b32bb9843
CinematicCow <CinematicCow@users.no-reply.github.com>
2021-04-21 00:12:32 +0530

    test: backend autdeploy 2

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'dfd8c255e92e0589599c4374359067ac'),
+      secret: env("ADMIN_JWT_SECRET", "dfd8c255e92e0589599c4374359067ac"),
     },
   },
 });
