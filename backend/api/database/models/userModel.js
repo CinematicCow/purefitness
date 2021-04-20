2db652827d901b16caff337c5a792b7467094e70
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2021-04-20 23:15:41 +0530

    Development (#4)
    * test: connection
    
    * test: webhooks
    
    * idk
    
    * Shifted to nextjs (#3)
    
    * test: intentional fail backend
    
    * test: fail backend deploy
    
    * test: pass backend deploy
    
    * test: idk
    
    * test: prod & discord
    
    * test: prod deploy fix
    
    * test
    
    * deleted frontend
    
    * add: frontend as next app
    
    * delete: backend
    
    * add: backend as strapi app
    
    * shifted to nextjs
    
    * added workflow
    
    * test: auto deploy to next
    
    * test: auto deploy to next 2
    
    * test: auto deploy to next 3
    
    * test auto deploy next 4
    
    * idk fuck
    
    * test: auto deploy next 5
    
    Co-authored-by: Prajwal <CinematicCow@users.no-reply.github.com>
    
    * test: dev auto deploy to netlify
    
    * test: auto deploy to dev netlifyy
    
    * test: deploy to dev
    
    * test: im an idiot?
    
    Co-authored-by: Prajwal <CinematicCow@users.noreply.github.com>
    Co-authored-by: Prajwal <CinematicCow@users.no-reply.github.com>

--- backend/api/database/models/userModel.js
+++
@@ -1,5 +0,0 @@
-// const mongoose = require("mongoose")
-
-// const programSchema = new mongoose.Schema({})
-
-// module.exports = mongoose.model("Program", programSchema)
---
+++ frontend/components/Navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar() {
+  return (
+    <div id="fh5co-header">
+      <header id="fh5co-header-section">
+        <div className="container">
+          <div className="nav-header">
+            <a href="#" className="js-fh5co-nav-toggle ">
+              <i />
+            </a>
+            <h1 id="fh5co-logo">
+              <a href="/">
+                <img src="images/pf-logo.png" width="80" alt="pf-logo" />
+                &nbsp; Pure<span>Fitness</span> &nbsp;&nbsp;
+              </a>
+            </h1>
+            {/* START #fh5co-menu-wrap */}
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Navbar;
